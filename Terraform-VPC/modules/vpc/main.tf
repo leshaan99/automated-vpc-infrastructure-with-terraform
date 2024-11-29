d2c8c340657f4d9fd5a55048adf26c329eff7577
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-11-29 20:07:33 +0530

    Create subnet for vpc

--- Terraform-VPC/modules/vpc/main.tf
+++ Terraform-VPC/modules/vpc/main.tf
@@ -6,3 +6,14 @@ resource "aws_vpc" "my_vpc" {
     "Name" = "my_vpc"
   }
 }
+
+#Subnet
+resource "aws_subnet" "subnets" {
+  count = length(var.subnet_cidr)
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = var.subnet_cidr[count.index]
+  availability_zone = data.aws_availability_zone.available.names[count.index]
+  tags = {
+    Name = var.subnet_names[count.index]
+  }
+}
