fb460457e8eda1e2056a9c9cc4053272af8bf6aa
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-11-29 10:09:47 +0530

    Update map public ip lunch in subnet

--- Terraform-VPC/modules/vpc/main.tf
+++ Terraform-VPC/modules/vpc/main.tf
@@ -9,12 +9,13 @@ resource "aws_vpc" "my_vpc" {
 }
 
 #Subnet
-resource "aws_subnet" "Subnets" {
+resource "aws_subnet" "subnets" {
   count = length(var.subnet_cidr)
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = var.subnet_cidr[count.index]
   availability_zone = data.aws_availability_zone.available.names[count.index]
-
+  map_public_ip_on_launch = true
+  
   tags = {
     Name = var.subnet_names[count.index]
   }
