4c38c674671303bf027c47804650208dc99b3cd4
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-11-29 10:17:29 +0530

    Create route table

--- Terraform-VPC/modules/vpc/main.tf
+++ Terraform-VPC/modules/vpc/main.tf
@@ -31,3 +31,10 @@ resource "aws_internet_gateway" "igw" {
 }
 
 #Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
