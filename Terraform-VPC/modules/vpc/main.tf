51e4ff1f2da4083b2a25ebb6dd0aa3517422cf85
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-11-29 21:46:34 +0530

    Create route table name

--- Terraform-VPC/modules/vpc/main.tf
+++ Terraform-VPC/modules/vpc/main.tf
@@ -36,6 +36,10 @@ resource "aws_route_table" "rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
+  tags = {
+    Name = "MyInternetGateway"
+  }
 }
 
 #Route Table Association
